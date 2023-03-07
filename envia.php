a5e950094ca190b82d6e8262b335e76400b54326
⚡ bzt 么 lezin™ ⚡ <106700535+txylezin@users.noreply.github.com>
2023-03-07 13:23:13 -0300

    Update envia.php

--- envia.php
+++ envia.php
@@ -4,10 +4,10 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'rei.geo@hotmail.com';
+$from = 'leoadro159@gmail.com';
 
 // an email address that will receive the email with the output of the form
-$sendo = 'oliveira.reinaldo@escola.pr.gov.br';
+$sendo = 'leandro.oliveira.silva@escola.pr.gov.br';
 
 // subject of the email
 $subject = 'Forumlário do site';
